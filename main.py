74afc2ca9ea7ce0e97bef9612449e0c89aa29452
안혜빈 <anhyepin@anhyebins-MacBook-Air.local>
2026-06-27 18:17:09 +0900

    260626_merge

--- main.py
+++ main.py
@@ -505,7 +505,6 @@ def run():
     start_time = time.time()
 
     config = {
-<<<<<<< HEAD
         "target_id": "32", # account_id
         "fb_ad_account_id":"act_1002942846241389",
         "start":"2026-04-13", #YYYY-MM-DD
@@ -513,15 +512,6 @@ def run():
         "main_age": ["25-34", "35-44"],
         "main_gender": "male", # male, female
         "avoid_age": "",
-=======
-        "target_id": "8", # account_id
-        "fb_ad_account_id":"act_1358317995522216",
-        "start":"2025-10-27", #YYYY-MM-DD
-        "end": "2026-05-31",
-        "main_age": ["35-44", "45-54"],
-        "main_gender": "female", # male, female
-        "avoid_age": "65+",
->>>>>>> main
         "avoid_gender": "",
     }
 
